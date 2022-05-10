7f21c3809f163d2fd1eede1c0af237c9e906ddee
Hesan Aminiloo <hesan.aminiloo@gmail.com>
2022-05-10 23:02:35 +0430

    Get phone provider with an extra flag.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const joi = require('joi');
 const extensions = require('./lib');
 
-const phoneNumber = '09123334433';
+// const phoneNumber = '09123334333';
+const phoneNumber = '۰۹33۷۷۸۸56۴';
 // const id = '0013681354';
-// const id = '۰۰۱۳۶۸۱۳۵۴';
+const id = '۰۰۱۳۶۸۱۳۵۴';
 
 const custom = joi.extend(...extensions);
 
 const schema = custom.object({
-  phone: custom.persianPhoneNumber(),
+  phone: custom.phone().with({ operator: true }).required().messages({
+    'phoneMsg.irancell': 'شماره ای که وارد کردید باید ایرانسل باشد',
+    'phoneMsg.characters': 'طول اعداد وارد شده باید دقیقا ۱۱ تا باشه',
+  }),
   id: custom.persianIdNumber(),
-});
+})
 
 const a = schema.validate({ phone: phoneNumber, id });
 console.log(a)
